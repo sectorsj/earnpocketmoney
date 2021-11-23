758439b32073bd24884d600c8410e6d46af06135
JohnnyGomezzz <seldesign@mail.ru>
2021-11-23 16:17:14 +0300

    Init project

---
+++ src/test/java/ru/coolteam/earnpocketmoney/EarnpocketmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coolteam.earnpocketmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarnpocketmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
