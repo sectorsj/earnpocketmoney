1a06dc98cd7567d8ad91e6978aed28691c9da146
KirillYrevichPushkin <benberov09@mail.ru>
2021-12-08 21:13:26 +0300

    Backendstep2 commit #1 Реализованы часть методов для child

--- src/main/java/ru/coolteam/earnpocketmoney/repositories/ChildRepository.java
+++ src/main/java/ru/coolteam/earnpocketmoney/repositories/ChildRepository.java
@@ -2,8 +2,15 @@ package ru.coolteam.earnpocketmoney.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.coolteam.earnpocketmoney.models.Bonus;
 import ru.coolteam.earnpocketmoney.models.Child;
+
+import java.util.Optional;
 
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Integer> {
+
+    Optional<Child> findChildByLogin (String login);
+
+
 }
--- src/main/java/ru/coolteam/earnpocketmoney/services/ChildService.java
+++ src/main/java/ru/coolteam/earnpocketmoney/services/ChildService.java
@@ -2,9 +2,11 @@ package ru.coolteam.earnpocketmoney.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.coolteam.earnpocketmoney.models.Bonus;
 import ru.coolteam.earnpocketmoney.models.Child;
 import ru.coolteam.earnpocketmoney.repositories.ChildRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +22,22 @@ public class ChildService {
     public List<Child> findAll() {
         return childRepository.findAll();
     }
+
+    public Optional<Child> findByLogin (String login) {
+        return childRepository.findChildByLogin(login);
+    }
+
+    public Child createChild (String login, String password){
+        Child child = new Child();
+        child.setLogin(login);
+        child.setPassword(password);
+        return childRepository.save(child);
+    }
+
+    public Child updateChild (String login, Integer wallet){
+        Child child = findByLogin(login).get();
+        child.setWallet(wallet);
+        return childRepository.save(child);
+    }
+
 }
